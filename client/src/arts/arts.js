1739c167e9149ab7b134c2843526b85368cf1696
Hong Tran <jessietran@fb.com>
2020-11-29 21:16:09 -0800

    added 1 art project

--- client/src/arts/arts.js
+++ client/src/arts/arts.js
@@ -9,5 +9,16 @@ export const arts = [
         code: "",
         link: "https://www.openprocessing.org/sketch/977579",
         created_at: "10/04/2020"
+    },
+    {
+        name: "Chicken",
+        image: "https://res.cloudinary.com/dfulxq7so/image/upload/v1606713242/Screen_Shot_2020-11-29_at_9.13.40_PM_salnj3.png",
+        description: "Hello world",
+        languages: [
+            "p5.js"
+        ],
+        code: "",
+        link: "https://www.openprocessing.org/sketch/995912",
+        created_at: "11/29/2020"
     }
 ]
